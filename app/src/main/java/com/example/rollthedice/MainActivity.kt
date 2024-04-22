57de5da96b7df726e46bdcf0175fe1f13e69b32a
2Kelvin <rocketmancodes02@gmail.com>
2024-04-22 22:25:30 +0300

    sketch out app + add images

--- app/src/main/java/com/example/rollthedice/MainActivity.kt
+++ app/src/main/java/com/example/rollthedice/MainActivity.kt
@@ -3,12 +3,17 @@ package com.example.rollthedice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.rollthedice.ui.theme.RollTheDiceTheme
 
@@ -23,9 +28,29 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun DiceWithButtonAndImage() {
 
+// passing a modifier parameter of type Modifier to style this Composable UI elements
+// the modifier parameter is assigned a default value of Modifier
+// one can pass a custom Modifier object to modifier param to style the UI their own way
+// if no value is passed to modifier param, the default Modifier object is used
+@Composable
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier
+    // taking the whole size of the screen
+    .fillMaxSize()
+    // wrapping around the contents and then placing them in the center (vertically & horizontally)
+    .wrapContentSize(Alignment.Center)
+) {
+    Column(
+        // passing the above modifier object with preset styles down to this child composable
+        // to make sure it adheres to the parent styles set in the modifier object
+        modifier = modifier,
+        // aligning column's elements to the center horizontally
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = stringResource(R.string.roll))
+        }
+    }
 }
 
 @Preview(showBackground = true)
